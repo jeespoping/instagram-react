e77c44bffbf5852554c0e0c43d5092ea11d5c0d1
meanuel <manuellopezflorez134@gmail.com>
2021-07-25 17:00:37 -0500

    validacione con yup

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useFormik } from 'formik'
 import "./LoginForm.scss"
+import * as Yup from 'yup';
 import {Form, Button} from 'semantic-ui-react'
 
 export default function LoginForm() {
     const formik = useFormik({
         initialValues: initialValue(),
-        validationSchema: null,
+        validationSchema: Yup.object({
+            emai: Yup.string().email("El email no es valido").required("El email es obligatorio"),
+            password: Yup.string().required("La contrase►4a es obligatoria"),
+        }),
 
         onSubmit: (FormData) => {
             console.log(FormData);
@@ -16,8 +20,8 @@ export default function LoginForm() {
     return (
         <Form onSubmit={formik.handleSubmit} className="login-form">
             <h2>Entra para ver fotos y videos de tus amigos</h2>
-            <Form.Input value={formik.values.email} onChange={formik.handleChange} type="text" placeholder="Correo electronico" name="email" />
-            <Form.Input value={formik.values.password} onChange={formik.handleChange} type="password" placeholder="Password" name="password" />
+            <Form.Input error={formik.errors.email && true} value={formik.values.email} onChange={formik.handleChange} type="text" placeholder="Correo electronico" name="email" />
+            <Form.Input error={formik.errors.password && true} value={formik.values.password} onChange={formik.handleChange} type="password" placeholder="Password" name="password" />
             <Button type="submit" className="btn-submit">
                 Iniciar sesion
             </Button>
