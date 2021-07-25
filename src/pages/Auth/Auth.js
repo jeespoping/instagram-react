d779707c0131c13e243bf05bd198417e6d79954e
meanuel <manuellopezflorez134@gmail.com>
2021-07-25 14:42:09 -0500

    maquetacion de login

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Image } from "semantic-ui-react";
+import instagram from '../../assets/logo.png';
 import "./Auth.scss";
 
 export default function Auth() {
+
+
+    const [showLogin, setShowLogin] =useState(true);
+
     return (
-        <div>
-            <h1>Estamos en AUTH</h1>
-        </div>
+        <Container fluid className="auth">
+            <Image src={instagram}></Image>
+
+            <div className="container-form">
+                {showLogin ? <p>Formulario de Login</p> : <p>Formulario de registro</p>}
+            </div>
+
+            <div className="change-form">
+                <p>
+                    {showLogin ? (
+                        <>
+                            No tienes cuenta?
+                            <span onClick={ () => setShowLogin(!showLogin) }>Registrate</span>
+                        </>
+                    ) : (
+                        <>
+                            Entra con tu cuenta!
+                            <span onClick={ () => setShowLogin(!showLogin) }>Iniciar sesion</span>
+                        </>
+                    )}
+                </p>
+            </div>
+        </Container>
     )
 }
