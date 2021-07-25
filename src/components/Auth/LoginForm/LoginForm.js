fd85bb033df2d128b7aed010f69778932a5582b7
meanuel <manuellopezflorez134@gmail.com>
2021-07-25 16:56:25 -0500

    añadiendo formik al formulario

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import { useFormik } from 'formik'
 import "./LoginForm.scss"
 import {Form, Button} from 'semantic-ui-react'
 
 export default function LoginForm() {
+    const formik = useFormik({
+        initialValues: initialValue(),
+        validationSchema: null,
+
+        onSubmit: (FormData) => {
+            console.log(FormData);
+        },
+    });
+
     return (
-        <Form className="login-form">
+        <Form onSubmit={formik.handleSubmit} className="login-form">
             <h2>Entra para ver fotos y videos de tus amigos</h2>
-            <Form.Input type="text" placeholder="Correo electronico" name="email" />
-            <Form.Input type="password" placeholder="Password" name="password" />
+            <Form.Input value={formik.values.email} onChange={formik.handleChange} type="text" placeholder="Correo electronico" name="email" />
+            <Form.Input value={formik.values.password} onChange={formik.handleChange} type="password" placeholder="Password" name="password" />
             <Button type="submit" className="btn-submit">
                 Iniciar sesion
             </Button>
         </Form>
     )
+}
+
+function initialValue() {
+    return {
+        email: "",
+        password: "",
+    }
 }
