894def0afe849a083096d3896c670f007cc43eef
meanuel <manuellopezflorez134@gmail.com>
2021-07-30 08:39:28 -0500

    creando las paginas

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect, useMemo } from 'react'
 import { ApolloProvider } from '@apollo/client'
 import client from './config/apollo';
 import Auth from './pages/Auth';
-import { ToastContainer } from 'react-toastify'
-import { getToken } from './utils/token'  
-import AuthContext from './context/AuthContext'
+import { ToastContainer } from 'react-toastify';
+import { getToken } from './utils/token';
+import AuthContext from './context/AuthContext';
+import Home from './pages/Home'
 
 export default function App() {
   const [auth, setAuth] = useState(undefined);
@@ -31,15 +32,15 @@ export default function App() {
     () => ({
       auth,
       logout,
-      setUser
+      setUser,
     }),
     [auth]
-  )
+  );
 
   return (
     <ApolloProvider client={ client }>
       <AuthContext.Provider value={authData}>      
-        {!auth ? <Auth /> : <h1>Estas logeado</h1>}
+        {!auth ? <Auth /> : <Home></Home>}
         <ToastContainer position="top-right" autoClose={5000} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover></ToastContainer>
       </AuthContext.Provider>    
     </ApolloProvider>
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Home.scss"
+
+export default function home() {
+
+    return (
+        <div>
+            <h1>Estamos en el home</h1>
+        </div>
+    )
+}
